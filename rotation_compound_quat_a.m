07266c3a2abf5a4eec000ce36d9364b66bd95388
Mike Kokko <32647624+treelinemike@users.noreply.github.com>
2020-05-31 12:53:55 -0400

    added a few tests to compound quaternion rotation script.

--- rotation_compound_quat_a.m
+++ rotation_compound_quat_a.m
@@ -1,8 +1,13 @@
+% test compound rotation with quaternions
+% for post-multiplying row vectors
+
 % restart
 close all; clear; clc;
 
+% define triad
 points = [0 0 0; 1 0 0; 0 0 0; 0 1 0; 0 0 0; 0 0 1];
 
+% configure plot and show initial triad position
 figure;
 hold on; grid on; axis equal;
 plot3(points(:,1),points(:,2),points(:,3),'k-','LineWidth',1.6);
@@ -24,8 +29,24 @@ theta2 = 10*pi/180;
 u2 = [0 1 0]';
 q2 = [cos(theta2/2); -sin(theta2/2)*u2]; % note negative sign b/c we'll be applying matrix to ROW VECTORS
 
+% assemble total rotation
 q = quatmult(q2,q1);
+R = quat2matrix(q);
 
-points_new = points*quat2matrix(q);
+% check doing combination in tangent space
+ss1 = theta1*[0 -u1(3) u1(2); u1(3) 0 -u1(1);-u1(2) u1(1) 0]';  % note transpose b/c row vectors (ugh)
+ss2 = theta2*[0 -u2(3) u2(2); u2(3) 0 -u2(1);-u2(2) u2(1) 0]';  % note transpose b/c row vectors (ugh)
+max(max(abs(quat2matrix(q1) - expm(ss1))))
+max(max(abs(quat2matrix(q2) - expm(ss2))))
+max(max(abs(expm(ss2)*expm(ss1) - R)))
 
+% Test Baker-Campbell-Hausdorff formula for combining matrix exponential
+% Not a good idea here because ss1 and ss2 are singular
+%max(max(abs( expm(ss2 + ss1 + .5*inv(ss2)*inv(ss1)*ss2*ss1 ) - R)))
+
+% perform total rotation
+% note: row vector is post multiplied by rotation matrix
+points_new = points*R;
+
+% show rotated triad
 plot3(points_new(:,1),points_new(:,2),points_new(:,3),'r-','LineWidth',1.6);
