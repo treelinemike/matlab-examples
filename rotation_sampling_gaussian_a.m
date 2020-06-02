5883c64b5090557b95f3334308bcaa4a7212281d
Mike Kokko <32647624+treelinemike@users.noreply.github.com>
2020-06-01 21:30:18 -0400

    updated visualization so lengths of sampled rotation axes are proportional to angle of rotation

--- rotation_sampling_gaussian_a.m
+++ rotation_sampling_gaussian_a.m
@@ -22,6 +22,7 @@ ts_mean = [ 0   -1.5459   1.5459]'; % tangent space coefficients of mean perturb
 sigma_x = (5)*pi/180;  % affects x component of rotation axis unit vector
 sigma_y = (5)*pi/180;  % affects y component of rotation axis unit vector
 sigma_z = (5)*pi/180;  % affects z component of rotation axis unit vector
+k_tang = 0.3;
 
 % data storage
 v_rot = zeros(3,N_samp);
@@ -60,8 +61,10 @@ for sampIdx = 1:size(samp,1)
    
    % convert to angle/axis and plot AXIS OF ROTATION for this sample
    angax = quat2angax(q_samp);
+   tang = quat2tang(q_samp);
    samp_angax(:,sampIdx) = angax;
-   plot3([0 angax(2)],[0 angax(3)],[0 angax(4)],'-','LineWidth',0.1,'Color',[0.8 0 0.8]);
+%    plot3([0 angax(2)],[0 angax(3)],[0 angax(4)],'-','LineWidth',0.1,'Color',[0.8 0 0.8]);
+   plot3(k_tang*[0 tang(1)],k_tang*[0 tang(2)],k_tang*[0 tang(3)],'-','LineWidth',0.1,'Color',[0.8 0 0.8]);
    
 end
 
@@ -91,7 +94,11 @@ plot3(samp_mean_vec(1),samp_mean_vec(2),samp_mean_vec(3),'.','MarkerSize',20,'Co
 % show the desired mean axis of rotation specified in the gaussian
 % distribution that we sample from
 ts_mean_uv = unitvec(ts_mean);
-plot3(ts_mean_uv(1),ts_mean_uv(2),ts_mean_uv(3),'.','MarkerSize',20,'Color',[0 0.8 0]);
+samp_mean_uv = unitvec(samp_mean);
+plot3([0 ts_mean_uv(1)],[0 ts_mean_uv(2)],[0 ts_mean_uv(3)],'-','Color',[0 0.8 0]);
+plot3(k_tang*ts_mean(1),k_tang*ts_mean(2),k_tang*ts_mean(3),'.','MarkerSize',20,'Color',[0 0.8 0]);
+plot3([0 samp_mean_uv(1)],[0 samp_mean_uv(2)],[0 samp_mean_uv(3)],'-','Color',[0 0 0]);
+plot3(k_tang*samp_mean(1),k_tang*samp_mean(2),k_tang*samp_mean(3),'.','MarkerSize',20,'Color',[0 0 0]);
 
 % show tangent space
 subplot(1,2,2);
