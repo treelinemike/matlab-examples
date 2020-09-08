dcf6bc25a25c09f824c8a48a14a29fa5052c89eb
Mike Kokko <32647624+treelinemike@users.noreply.github.com>
2020-09-08 18:23:40 -0400

    continued developing linear least squares demo including plotting optiizer performance

--- linear_least_squares_demo_a.m
+++ linear_least_squares_demo_a.m
@@ -2,7 +2,6 @@
 close all; clear; clc;
 
 % options
-doShowWeightedSoln = 1;
 doRunAnimation = 0;
 doMakeVideo = 0;
 outputFilename = 'lsq_demo_a.mp4';
@@ -19,20 +18,16 @@ meanpt = mean([t f]);
 SST = (f-meanpt(2))'*(f-meanpt(2));  % SST = SSE for model that is just mean of data
 
 % solve with MATLAB mldivide()
-x_hat_1 = A\f
+x_opt_mldivide = A\f
 
 % use MATLAB fitlm()
 mdl = fitlm(t,f)
 
 % or do it the long way
-x_hat_2 = (A'*A)\(A'*f)     % x_hat = inv(A'*A)*A'*f
-resid_2 = f-A*x_hat_2;
-SSE_2 = resid_2'*resid_2;
-rsq_2 = (SST-SSE_2)/SST
-
-% compute the weighted solution
-COV_f = (1/(length(f)-1))*(f-mean(f))*(f-mean(f))';
-WR = inv(COV_f);  % ideally compute this using eigendecomposition, or similar
+x_opt_analytic = (A'*A)\(A'*f)     % x_hat = inv(A'*A)*A'*f
+resid_analytic = f-A*x_opt_analytic;
+SSE_analytic = resid_analytic'*resid_analytic;
+rsq_analytic = (SST-SSE_analytic)/SST
 
 
 % initialize plot
@@ -78,9 +73,9 @@ for stepIdx = 1:length(theta_vals);
 
     % plot residuals
     cla;
-%     for residIdx = 1:length(resid)
-%         ph(3) = plot(t(residIdx)*ones(1,2),f(residIdx)-[0 resid(residIdx)],'-','LineWidth',1.6,'Color',[0.8 0 0]);
-%     end
+    for residIdx = 1:length(resid)
+        ph(3) = plot(t(residIdx)*ones(1,2),f(residIdx)-[0 resid(residIdx)],'-','LineWidth',1.6,'Color',[0.8 0 0]);
+    end
     
     % determine how to plot current and best fit lines
     if(m < 1)
@@ -105,7 +100,7 @@ for stepIdx = 1:length(theta_vals);
     ph(1) = plot(t,f,'.','MarkerSize',30,'Color',[0 0 0.8]);
 
     % plot current and best fit lines
-%     ph(2) = plot(xvals,yvals,'-','LineWidth',3,'Color',[0 0.8 0]);
+    ph(2) = plot(xvals,yvals,'-','LineWidth',3,'Color',[0 0.8 0]);
     if(doRunAnimation)
         ph(4) = plot(min_xvals,min_yvals,':','LineWidth',3,'Color',[0.8 0 0.8]);
     end
@@ -139,3 +134,98 @@ end
 %% check calculations that mean of data lies on regression line
 aa = 1/( (length(t))*sum(t.^2)-(sum(t))^2 ) * [length(t) -sum(t);-sum(t) sum(t.^2) ];
 (1/length(t))*[sum(t) length(t)]*aa*[sum(t.*f); sum(f)];
+
+%% compute actual cost function
+x1_vals = -0.5:0.05:2;
+x2_vals = -5:0.1:5;
+[X1,X2] = meshgrid(x1_vals,x2_vals);
+J = zeros(size(X1));
+for elIdx = 1:numel(X1)
+   x = [X1(elIdx) X2(elIdx)]';
+   J(elIdx) = calcSSE(x,A,f);
+end
+
+figure;
+set(gcf,'Position',[2.178000e+02 2.458000e+02 1.003200e+03 4.200000e+02]);
+subplot(1,2,1);
+hold on; grid on;
+view([78,54]);
+surf(X1,X2,J);
+xlabel('\bfSlope');
+ylabel('\bfIntercept');
+zlabel('\bfCost');
+plot3(x_opt_analytic(1),x_opt_analytic(2),SSE_analytic,'.','MarkerSize',30,'Color',[0.8 0 0.8]);
+
+%% now try optimization
+
+
+
+% wrap error function to pass parameters/data
+optfunc = @(x)calcSSE(x,A,f);
+
+% optimization over both slope and intercept parameters
+[x_opt_fminsearch,sse_opt_fminsearch] = fminsearch(optfunc,[0 0]');
+
+% using fminunc
+% opts = optimoptions('fminunc','Display','iter','PlotFcn','optimplotfval');
+opts = optimoptions('fminunc','Display','iter','OutputFcn',@myoutfun);
+[x_opt_fminunc,sse_opt_fminunc] = fminunc(optfunc,[0 0]',opts);
+allData = myoutfun([],[],[],1);
+allData(1,:) = [];
+allData(end,:) = [];
+disp(allData);
+plot3(allData(:,1),allData(:,2),allData(:,3),'.-','LineWidth',2,'Color',[0.8 0 0],'MarkerSize',20);
+
+% using fmincon with equality constraint forcing regression
+% through mean of data
+% note: x0 = [0 0]' does NOT satisfy constraints so fmincon adjusts it...
+% or we can start with, say, the constraint minimum norm solution
+Aeq = [mean(t) 1];
+beq = mean(f);
+% x0_con = Aeq'*inv(Aeq*Aeq')*beq;
+x0_con = [0 0]';
+% opts = optimoptions('fmincon','Display','iter-detailed','PlotFcn','optimplotfval','OutputFcn',@myoutfun);
+
+xc2_vals = (beq-x1_vals*Aeq(1))/Aeq(2);
+xc = [x1_vals; xc2_vals];
+Jc = zeros(1,size(xc,2));
+for cIdx = 1:size(xc,2)
+   Jc(cIdx) = calcSSE(xc(:,cIdx),A,f); 
+end
+plot3(xc(1,:),xc(2,:),Jc,'-','LineWidth',3,'Color',[0.8 0 0.8]);
+
+opts = optimoptions('fmincon','Display','iter','OutputFcn',@myoutfun);
+[x_opt_fmincon,sse_opt_fmincon,~,output] = fmincon(optfunc,x0_con,[],[],Aeq,beq,[],[],[],opts);
+allData = myoutfun([],[],[],1);
+allData(1,:) = [];
+allData(end,:) = [];
+disp(allData);
+plot3(allData(:,1),allData(:,2),allData(:,3),'.-','LineWidth',2,'Color',[0 0.8 0],'MarkerSize',20);
+xlim([ min(x1_vals), max(x1_vals)]);
+ylim([ min(x2_vals), max(x2_vals)]);
+
+subplot(1,2,2);
+hold on; grid on;
+xlabel('\bfPosition Along Constraint');
+ylabel('\bfCost');
+title('\bfConstrained View');
+posAlongConstraint = sqrt((xc(1,:)-xc(1,1)).^2 +(xc(2,:)-xc(2,1)).^2 );
+optPosAlongConstraint = sqrt((allData(:,1)-xc(1,1)).^2 +(allData(:,2)-xc(2,1)).^2 )
+plot(posAlongConstraint,Jc,'LineWidth',3,'Color',[0.8 0 0.8]);
+plot(optPosAlongConstraint,allData(:,3),'.-','LineWidth',2,'MarkerSize',30,'Color',[0 0.8 0]);
+xlim([0,max(posAlongConstraint)]);
+function stop = myoutfun(x,optimValues,state,getData)
+    persistent optData
+    
+    if(nargin == 4 && getData)
+        stop = optData;
+        optData = [];
+    else
+        optData(end+1,:) = [x' optimValues.fval];
+        stop = false;
+    end
+end
+
+function sse = calcSSE(x,A,b)
+    sse = (b-A*x)'*(b-A*x);
+end
