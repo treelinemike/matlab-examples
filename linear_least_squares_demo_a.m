e7880dab47567afcd660d3b0de2c8c5a5b8a5c51
Mike Kokko <32647624+treelinemike@users.noreply.github.com>
2020-09-09 15:55:35 -0400

    overhauled plotting of optimization

--- linear_least_squares_demo_a.m
+++ linear_least_squares_demo_a.m
@@ -136,8 +136,8 @@ aa = 1/( (length(t))*sum(t.^2)-(sum(t))^2 ) * [length(t) -sum(t);-sum(t) sum(t.^
 (1/length(t))*[sum(t) length(t)]*aa*[sum(t.*f); sum(f)];
 
 %% compute actual cost function
-x1_vals = -0.5:0.05:2;
-x2_vals = -5:0.1:5;
+x1_vals = -0.5:0.1:2;
+x2_vals = -5:0.2:5;
 [X1,X2] = meshgrid(x1_vals,x2_vals);
 J = zeros(size(X1));
 for elIdx = 1:numel(X1)
@@ -146,15 +146,17 @@ for elIdx = 1:numel(X1)
 end
 
 figure;
-set(gcf,'Position',[2.178000e+02 2.458000e+02 1.003200e+03 4.200000e+02]);
-subplot(1,2,1);
+set(gcf,'Position',[3.674000e+02 0217 1.023200e+03 4.608000e+02]);
+subplot(2,2,[1,3]);
 hold on; grid on;
-view([78,54]);
-surf(X1,X2,J);
+view([234,44]);
+sh = surf(X1,X2,J);
+ph2(2) = plot3(x_opt_analytic(1),x_opt_analytic(2),SSE_analytic,'.','MarkerSize',40,'Color',[0.8 0 0.8]);
 xlabel('\bfSlope');
 ylabel('\bfIntercept');
 zlabel('\bfCost');
-plot3(x_opt_analytic(1),x_opt_analytic(2),SSE_analytic,'.','MarkerSize',30,'Color',[0.8 0 0.8]);
+set(sh,'FaceAlpha',0.6);
+set(gca,'DataAspectRatio',[1 4 400])
 
 %% now try optimization
 
@@ -166,15 +168,6 @@ optfunc = @(x)calcSSE(x,A,f);
 % optimization over both slope and intercept parameters
 [x_opt_fminsearch,sse_opt_fminsearch] = fminsearch(optfunc,[0 0]');
 
-% using fminunc
-% opts = optimoptions('fminunc','Display','iter','PlotFcn','optimplotfval');
-opts = optimoptions('fminunc','Display','iter','OutputFcn',@myoutfun);
-[x_opt_fminunc,sse_opt_fminunc] = fminunc(optfunc,[0 0]',opts);
-allData = myoutfun([],[],[],1);
-allData(1,:) = [];
-allData(end,:) = [];
-disp(allData);
-plot3(allData(:,1),allData(:,2),allData(:,3),'.-','LineWidth',2,'Color',[0.8 0 0],'MarkerSize',20);
 
 % using fmincon with equality constraint forcing regression
 % through mean of data
@@ -186,34 +179,64 @@ beq = mean(f);
 x0_con = [0 0]';
 % opts = optimoptions('fmincon','Display','iter-detailed','PlotFcn','optimplotfval','OutputFcn',@myoutfun);
 
+
+
 xc2_vals = (beq-x1_vals*Aeq(1))/Aeq(2);
 xc = [x1_vals; xc2_vals];
 Jc = zeros(1,size(xc,2));
 for cIdx = 1:size(xc,2)
    Jc(cIdx) = calcSSE(xc(:,cIdx),A,f); 
 end
-plot3(xc(1,:),xc(2,:),Jc,'-','LineWidth',3,'Color',[0.8 0 0.8]);
+ph2(5) = plot3(xc(1,:),xc(2,:),Jc,'-','LineWidth',4,'Color',[0.8 0 0.8]);
 
 opts = optimoptions('fmincon','Display','iter','OutputFcn',@myoutfun);
 [x_opt_fmincon,sse_opt_fmincon,~,output] = fmincon(optfunc,x0_con,[],[],Aeq,beq,[],[],[],opts);
-allData = myoutfun([],[],[],1);
-allData(1,:) = [];
-allData(end,:) = [];
-disp(allData);
-plot3(allData(:,1),allData(:,2),allData(:,3),'.-','LineWidth',2,'Color',[0 0.8 0],'MarkerSize',20);
+allData_con = myoutfun([],[],[],1);
+allData_con(1,:) = [];
+allData_con(end,:) = [];
+disp(allData_con);
+ph2(4) = plot3(allData_con(:,1),allData_con(:,2),allData_con(:,3),'.-','LineWidth',3,'Color',[0 0.8 0],'MarkerSize',30);
 xlim([ min(x1_vals), max(x1_vals)]);
 ylim([ min(x2_vals), max(x2_vals)]);
 
-subplot(1,2,2);
+% using fminunc
+% opts = optimoptions('fminunc','Display','iter','PlotFcn','optimplotfval');
+opts = optimoptions('fminunc','Display','iter','OutputFcn',@myoutfun);
+[x_opt_fminunc,sse_opt_fminunc] = fminunc(optfunc,[0 0]',opts);
+allData_unc = myoutfun([],[],[],1);
+allData_unc(1,:) = [];
+allData_unc(end,:) = [];
+disp(allData_unc);
+ph2(3) = plot3(allData_unc(:,1),allData_unc(:,2),allData_unc(:,3),'.-','LineWidth',3,'Color',[0.8 0 0],'MarkerSize',30);
+ph2(1) = plot3(0,0,calcSSE([0;0],A,f),'.','MarkerSize',50,'Color',[0 0.5 0]);
+lh = legend(ph2,'Start','Truth','fminunc','fmincon','Constrained Cost');
+set(lh,'Position',[0.3465 0.6915 0.0873 0.1866]);
+
+
+% plot constrained view
+subplot(2,2,2);
 hold on; grid on;
 xlabel('\bfPosition Along Constraint');
 ylabel('\bfCost');
 title('\bfConstrained View');
 posAlongConstraint = sqrt((xc(1,:)-xc(1,1)).^2 +(xc(2,:)-xc(2,1)).^2 );
-optPosAlongConstraint = sqrt((allData(:,1)-xc(1,1)).^2 +(allData(:,2)-xc(2,1)).^2 )
-plot(posAlongConstraint,Jc,'LineWidth',3,'Color',[0.8 0 0.8]);
-plot(optPosAlongConstraint,allData(:,3),'.-','LineWidth',2,'MarkerSize',30,'Color',[0 0.8 0]);
+optPosAlongConstraint = sqrt((allData_con(:,1)-xc(1,1)).^2 +(allData_con(:,2)-xc(2,1)).^2 )
+ph3(2) = plot(posAlongConstraint,Jc,'LineWidth',3,'Color',[0.8 0 0.8]);
+plot(norm(x_opt_analytic-xc(:,1)),SSE_analytic,'.','MarkerSize',50,'Color',[0.8 0 0.8]);
+ph3(1) = plot(optPosAlongConstraint,allData_con(:,3),'.-','LineWidth',2,'MarkerSize',30,'Color',[0 0.8 0]);
 xlim([0,max(posAlongConstraint)]);
+legend(ph3,'fmincon','Constrained Cost','Location','NorthWest');
+% plot cost vs. iterations
+subplot(2,2,4);
+semilogy(1:size(allData_unc,1),allData_unc(:,3),'.-','MarkerSize',20,'LineWidth',1.6,'Color',[0.8 0 0]);
+hold on; grid on;
+semilogy(1:size(allData_con,1),allData_con(:,3),'.-','MarkerSize',20,'LineWidth',1.6,'Color',[0 0.8 0]);
+legend('fminunc','fmincon');
+xlabel('\bfNumber of Iterations');
+ylabel('\bfCost');
+
+% function get details of optimization steps
+% TODO: this is really a hack, but it mostly works...
 function stop = myoutfun(x,optimValues,state,getData)
     persistent optData
     
