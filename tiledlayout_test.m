06fc420b3997455c6da7fedb1025a79c8c4f6855
Mike Kokko <32647624+treelinemike@users.noreply.github.com>
2021-04-22 21:17:13 -0400

    updated with new spacing options in R2021a as well as super labels

--- tiledlayout_test.m
+++ tiledlayout_test.m
@@ -1,5 +1,5 @@
 % potentially life-changing replacement for subplot?
-% added in R2019b
+% added in R2019b; options revised in 
 
 % restart
 close all; clear; clc;
@@ -13,16 +13,37 @@ figure;
 set(gcf,'Position',[1.898000e+02 1.602000e+02 7.312000e+02 5.248000e+02]);  % adjust as necessary
 t = tiledlayout(numRows,numCols);
 
+% get rid of that pesky whitespace!
+t.Padding = 'loose';  % 'loose', 'compact', or 'tight'
+t.TileSpacing = 'tight';  % 'loose', 'compact', 'tight', or 'none'
+
+
 % add individual tiles
+ax = [];
 for rowIdx = 1:numRows
     for colIdx = 1:numCols
         thisTileIdx = (rowIdx-1)*numCols+colIdx;
-        nexttile(thisTileIdx);
+        ax(end+1) = nexttile(thisTileIdx);
         hold on; grid on;
         spy(eye(thisTileIdx),50);
+        if(rowIdx == 1)
+            set(ax(end),'XTickLabel',{});
+            xlabel('');
+        else
+            xlabel('\bfX Axis Label');
+        end
+        if(colIdx == 1)
+            ylabel('\bfY Axis Label');
+        else
+            set(ax(end),'YTickLabel',{});
+        end
     end
 end
 
-% get rid of that pesky whitespace!
-t.Padding = 'none';  % 'normal', 'compact', or 'none'
-t.TileSpacing = 'none';  % 'normal', 'compact', or 'none'
+% link axes
+linkaxes(ax,'xy');
+
+% add titles
+title(t,'\bfSuper Title');
+xlabel(t,'\bfSuper X Axis');
+ylabel(t,'\bfSuper Y Axis');
