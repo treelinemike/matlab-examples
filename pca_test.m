ce386456498ef6bc0bb9a13e632fa3768aca710c
Mike Kokko <32647624+treelinemike@users.noreply.github.com>
2021-08-23 15:49:29 -0400

    changed pca test to data examples in rows and dimensions along columns

--- pca_test.m
+++ pca_test.m
@@ -1,45 +1,50 @@
+% Test three related methods of performing basic PCA
+% Author: M. Kokko
+% Updated: 23-Aug-2021
+
 % restart
-close all; clear all; clc;
+close all; clear all; clc; rng('default');
 
-% define test matrix
-% pts_ex1 = [1 1 1; 2 1 3; 4.5 1 1; 7 -2 4; 6 3 -2; 4 8 -2; -10 2 5];
-% pts_ex2 = [0 -2 7; 3 1 1.3; 8 4 0.3; 4 2.3 11; 7 0.1 -18; 6.5 7 -11; -2.8 4 -9];
-% pts_ex3 = [3 3 21; 4 -1 5; -2 3.34 17; 1 16 -2; 0.9 3 4.2; 15 0.2 12; -2.1 -6 5.6];
-% pts_ex4 = [5 -5 4.1; 5 -1 9; 6 1 -2.5; 9 -8 -2; 1 11.2 6; -12 3.6 14; 7.4 7 2];
-% 
-% x = [ pts_ex1(:), pts_ex2(:), pts_ex3(:), pts_ex4(:) ]; 
+% generate a random data matrix
+M = 8;
+N = 7000;
+X = randn(M,N);
 
-x = [1 2 3 4; 3 1 6 4; 5 -2 4 -2]';
-x = randn(7000,8);
+% compute covariance matrices
+C = eye(M) - (1/M)*ones(M);
+A = C*X;
+COV1 = (1/(M-1))*((A')*A); % PARENTHESES VERY IMPORTANT! 
+COV2 = (1/(M-1))*(A*(A')); % PARENTHESES VERY IMPORTANT!
 
-s = size(x,2);
-C = eye(s) - (1/s)*ones(s);
-A = x*C;
-COV1 = (1/(s-1))*(A*(A')); % NOTE: PARENTHESES ARE VERY IMPORTANT AROUND x*x' !!! WILL GO COMPLEX WITHOUT THEM!
-COV2 = (1/(s-1))*((A')*A);
-
-% run eigendecomposition
-
+% method #1: eigendecomposition of (A')*A
 tic
 [V1,D1] = eig(COV1);
 [d1,ind1] = sort(diag(D1),'descend');
 D1s = D1(ind1,ind1);
 V1s = V1(:,ind1);
-V1s = V1s*diag(sign(V1s(1,:))); % preference for first component of eigenvectors to be positive
+V1s = V1s*diag(sign(V1s(1,:))); % choose first component positive
 toc;
 
+% method #2: eigendecomposition of A*(A')
 tic
 [V2,D2] = eig(COV2);
 [d2,ind2] = sort(diag(D2),'descend');
 D2s = D2(ind2,ind2);
-V2s = unitvec(A*V2(:,ind2));
-V2s = V2s*diag(sign(V2s(1,:)));
+V2s = unitvec((A')*V2(:,ind2)); % normalize for unit vector columns
+V2s = V2s*diag(sign(V2s(1,:))); % choose first component positive
 toc;
 
+% method #3: decompose A using the SVD
 tic
 [U,S,V] = svd(A);
 [d3,ind3] = sort(diag(S),'descend');
-D3s = S(ind3,ind3);
-V3s = unitvec(U(:,ind2));
-V3s = V3s*diag(sign(V3s(1,:)));
+D3s = (1/(M-1))*S(ind3,ind3).^2;
+V3s = V(:,ind3);
+V3s = V3s*diag(sign(V3s(1,:))); % choose first component positive
 toc;
+
+% compare eigenvalues and eigenvectors
+query_idx = 1;
+[D1s(query_idx,query_idx) D2s(query_idx,query_idx) D3s(query_idx,query_idx)]
+[V1s(1:8,query_idx) V2s(1:8,query_idx) V3s(1:8,query_idx)]
+ 
