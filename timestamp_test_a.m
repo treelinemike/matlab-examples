f2cacea34f320d06c965dcd93361193ded5cc4ea
Mike Kokko <32647624+treelinemike@users.noreply.github.com>
2021-10-25 12:17:50 -0400

    fixed comment error

--- timestamp_test_a.m
+++ timestamp_test_a.m
@@ -11,7 +11,7 @@ disp(['Current datetime:      ' datestr(currentDateTime)]);
 currentUnixTime = posixtime(currentDateTime);
 fprintf('Current UNIX time:     %20.9f\n',currentUnixTime);  % display to nanoseconds, not sure about actual underlying accuracy
 
-% convert UNIX time back to MATLAB datetimedt
+% convert UNIX time back to MATLAB datetime
 % assuming UNIX time was reported in UTC
 convertedDateTime = datetime(currentUnixTime,'ConvertFrom','posixtime','TimeZone','UTC');
 disp(['Converted datetime:    ' datestr(convertedDateTime)]);
