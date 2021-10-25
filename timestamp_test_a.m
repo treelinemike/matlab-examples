701b2de8152986a138c897e20e7746c73eab9253
Mike Kokko <32647624+treelinemike@users.noreply.github.com>
2021-10-25 11:21:02 -0400

    added timestamp test (for UNIX timestamp generation and conversion

---
+++ timestamp_test_a.m
@@ -0,0 +1,21 @@
+% test some matlab time conversions
+
+% restart
+close all; clear; clc;
+
+% get current time as a UNIX timestamp
+% make sure to get datetime in UTC (vs local time zone)
+% can verify against: https://www.unixtimestamp.com/index.php
+currentDateTime = datetime('now','TimeZone','UTC');
+disp(['Current datetime:      ' datestr(currentDateTime)]);
+currentUnixTime = posixtime(currentDateTime);
+fprintf('Current UNIX time:     %20.9f\n',currentUnixTime);  % display to nanoseconds, not sure about actual underlying accuracy
+
+% convert UNIX time back to MATLAB datetime
+% assuming UNIX time was reported in UTC
+convertedDateTime = datetime(currentUnixTime,'ConvertFrom','posixtime','TimeZone','local');
+disp(['Converted datetime:    ' datestr(convertedDateTime)]);
+
+% get offset from UTC (i.e. GMT)
+localTZOffset = tzoffset(datetime('today','TimeZone','local'));
+disp(['Local offset from UTC: ' char(localTZOffset)]);
